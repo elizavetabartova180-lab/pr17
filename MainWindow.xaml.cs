d763eb715ff2e17c933f2720d911192a3bc891a7
ADMIN <ADMIN@MSI>
2026-01-22 03:50:43 +0500

    Созданный класс Dish. Реализация перехода на окно Main. Bartova

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using System.IO;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Pizza_Bartova
 {
@@ -20,11 +9,22 @@ namespace Pizza_Bartova
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string localPath;
         public MainWindow()
         {
             InitializeComponent();
             frame.Navigate(new MainWindow());
+            localPath = System.IO.Directory.GetCurrentDirectory();
+            OpenPages(pages.main);
         }
-        
+        public enum pages
+        {
+            main,
+        }
+        public void OpenPages(pages _pages)
+        {
+            if (_pages == pages.main)
+                frame.Navigate(new Layouts.Main(this));
+        }
     }
 }
